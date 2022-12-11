4a82f6f3afced62ca5568c0f651e8987778a2e09
Vinícius Rodrigues <viniciusrf2@al.insper.edu.br>
2022-12-11 11:25:02 -0300

    Questão "Classificador de triângulos"

--- Aula 04/classifica_triangulo.py
+++ Aula 04/classifica_triangulo.py
@@ -1,3 +1,5 @@
+# Questão "Classificador de triângulos"
+
 def classifica_triangulo(a, b, c):
     if(a == b and b == c): return "equilátero"
     elif(a == b or b == c or a == c): return "isósceles"
